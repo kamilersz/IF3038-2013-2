e004ab06923b1023a9630c90011b9325edcfd2b7
Irfan Kamil <kmz.in.act@gmail.com>
2013-03-08 21:48:46 +0700

    php configuration
    query function and db handler

---
+++ src/core/config.php
@@ -0,0 +1,39 @@
+<?php
+
+define ('USERID','$_SESSION["user_id"]');
+define ('QUERY','$stmt = $conn->prepare');
+define ('EXECUTE','$stmt->execute');
+define ('FETCH','$stmt->fetch()');
+
+function getUserID() {
+	return (isset($_SESSION["user_id"])?$_SESSION["user_id"]:0);
+}
+
+function query($statement,$cond) {
+	$conn = Db::handler();
+	$stmt = $conn->prepare($statement);
+	$stmt->execute($cond);
+	return $stmt->fetch();
+}
+
+class Db
+{
+	private static $db;
+	
+	public static function handler()
+	{
+		if (!self::$db)
+		{
+			try {
+				self::$db = new PDO('mysql:host=localhost;dbname=progin_405_13510001', 'progin', 'progin');
+				self::$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				self::$db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+			} catch (PDOException $e) {
+				die('Connection error: ' . $e->getMessage());
+			}
+		}
+		return self::$db;
+	}
+}
+
+?>
