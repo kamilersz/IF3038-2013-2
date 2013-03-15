b323d41b4af1b12c13c531929297ee836314c44a
Irfan Kamil <kmz.in.act@gmail.com>
2013-03-15 21:42:54 +0700

    added 'new category' menu
    (still stub button)

--- src/dashboard.php
+++ src/dashboard.php
@@ -16,8 +16,29 @@
 					<h1>Dashboard</h1>
 					<ul>
 						<li class="add-task-link"><a href="new_tugas.php">New Task</a></li>
+						<li class="login"><a href="#" id="loginLink">New Category</a></li>
 					</ul>
 				</header>
+				
+				<section class="login-box" id="loginBox">
+					<header>
+						<h3>New Category</h3>
+					</header>
+					<form id="newCategoryForm" action="#" method="post" class="vertical">
+						<div class="field">
+							<label>Name</label>
+							<input size="30" maxlength="50" name="name" id="category_name" type="text">
+						</div>
+						<div class="field">
+							<label>Assignee</label>
+							<input size="30" maxlength="50" name="assignee[]" id="login_password" type="password">
+						</div>
+						<div class="field">
+							<button type="submit" id="loginButton">Login</button>
+						</div>
+					</form>
+				</section>
+				
 				<div class="primary">
 					<section class="tasks current" id="activeTask">
 					
