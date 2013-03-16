fe79354f514bfd19f09c7a71d72c331437082bb4
Hanif Eridaputra <he.23292@gmail.com>
2013-03-16 23:28:04 +0700

    .

--- src/core/updateUsername.php
+++ src/core/updateUsername.php
@@ -4,9 +4,7 @@
 	var_dump($post);
 	$username = $post['username'];
 	$user_id = $post['user_id']
-	$hasil = queryn('UPDATE user SET username = :username WHERE user_id = :user_id',array(
-		'task_id' => $task_id
-		));
+	$hasil = queryn('UPDATE user SET username = :username WHERE user_id = :user_id',array());
 ?>
 
 <!-- Ini bener gak? cara manggilnya gimana? -->
